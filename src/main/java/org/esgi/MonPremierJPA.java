c3ae9d295c73cd6589b6389d15d2eeffc50d7f6b
Freddy Chiron <freddy.chiron@gmail.com>
2019-06-28 16:38:34 +0200

    1ere version du tp

--- src/main/java/org/esgi/MonPremierJPA.java
+++ src/main/java/org/esgi/MonPremierJPA.java
@@ -8,29 +8,14 @@ public  class MonPremierJPA {
 
     public  static  void main(String... args) {
 
-        EntityManagerFactory emf = Persistence.createEntityManagerFactory("jpa-test") ;
 
-        EntityManager em = emf.createEntityManager() ;
-
-        Book book =  new Book() ;
-        em.getTransaction().begin() ;
-        em.persist(book) ;
-        em.getTransaction().commit() ;
-
-        System.out.println("Id = " + book.getId()) ;
+        // TODO creer entity manager
 
 
-        Book book1 = new Book();
-        book1.setTitle("DSdsdssd");
-        em.getTransaction().begin() ;
-        em.persist(book1) ;
+        // TODO creer un nouveau book
+        //TODO sauvegarder le book
 
-        em.getTransaction().commit() ;
-
-        System.out.println("Id = " + book1.getId()) ;
-
-        Book book2 = em.find(Book.class, book1.getId());
-        System.out.println(book2.getTitle());
+        // TODO afficher l'id du book sauvegardé
 
     }
 }
